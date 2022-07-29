fb313d4ba0ccad561373517db11d8d3742dc56b7
JouriR <jouri.roosjen@home.nl>
2022-07-29 10:14:58 +0200

    Fixed link to backend

--- pages/login.tsx
+++ pages/login.tsx
@@ -13,7 +13,7 @@ const Login: NextPage = () => {
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <form className="space-y-6" action={getApiLink() + "/login"} method="POST">
+            <form className="space-y-6" action={getApiLink() + "login"} method="POST">
               <div>
                 <label
                   htmlFor="email"
