9c59a61c667dfcd0815b6665fc65b46914caba35
JouriR <jouri.roosjen@home.nl>
2022-07-31 14:35:19 +0200

    Fixed auth hook

--- hooks/auth.ts
+++ hooks/auth.ts
@@ -2,19 +2,17 @@ import useSWR from "swr";
 import axios from "../lib/axios";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import { getApiLink } from "../imports/functions";
 
 export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
-  const apiLink = getApiLink();
   const router = useRouter();
 
   const {
     data: user,
     error,
     mutate,
-  } = useSWR(`${apiLink}/user`, () =>
+  } = useSWR("/api/user", () =>
     axios
-      .get(`${apiLink}/user`)
+      .get("/api/user")
       .then((res) => res.data)
       .catch((error) => {
         if (error.response.status !== 409) throw error;
