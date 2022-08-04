5a35424b6e79e59e1b52cc20d8c0c5b397650014
JouriR <jouri.roosjen@home.nl>
2022-08-04 14:06:56 +0200

    Added front end

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -4,11 +4,98 @@ import Layout from "../components/Layout";
 import Head from "next/head";
 
 const Projects: NextPageWithLayout = () => {
+  const people = [
+    {
+      name: "Lindsay Walton",
+      title: "Front-end Developer",
+    },
+    {
+      name: "Lindsay Walton",
+      title: "Front-end Developer",
+    },
+    {
+      name: "Lindsay Walton",
+      title: "Front-end Developer",
+    },
+    {
+      name: "Lindsay Walton",
+      title: "Front-end Developer",
+    },
+  ];
+
   return (
     <>
       <Head>
         <title>Dashboard | Projects</title>
       </Head>
+
+      <div className="px-4 sm:px-6 lg:px-8">
+        <div className="sm:flex sm:items-center">
+          <div className="sm:flex-auto">
+            <h1 className="text-xl font-semibold text-gray-900">Projects</h1>
+            <p className="mt-2 text-sm text-gray-700">
+              A list of all the projects.
+            </p>
+          </div>
+          <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+            <button
+              type="button"
+              className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+            >
+              Create Project
+            </button>
+          </div>
+        </div>
+        <div className="-mx-4 mt-8 overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:-mx-6 md:mx-0 md:rounded-lg">
+          <table className="min-w-full divide-y divide-gray-300">
+            <thead className="bg-gray-50">
+              <tr>
+                <th
+                  scope="col"
+                  className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                >
+                  Title
+                </th>
+                <th
+                  scope="col"
+                  className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                >
+                  ID
+                </th>
+                <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                  <span className="sr-only">Edit</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-200 bg-white">
+              {people.map((person) => (
+                <tr key={person.title}>
+                  <td className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-6">
+                    {person.name}
+                    <dl className="font-normal lg:hidden">
+                      <dt className="sr-only">Title</dt>
+                      <dd className="mt-1 truncate text-gray-700">
+                        {person.title}
+                      </dd>
+                    </dl>
+                  </td>
+                  <td className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">
+                    {person.title}
+                  </td>
+                  <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                    <a
+                      href="#"
+                      className="text-indigo-600 hover:text-indigo-900"
+                    >
+                      Edit<span className="sr-only">, {person.name}</span>
+                    </a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   );
 };
